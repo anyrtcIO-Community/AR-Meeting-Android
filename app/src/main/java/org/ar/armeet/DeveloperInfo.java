35b7cef18de84f5b489062482e5f918d30c05da1
liuxiaozhong <liuxiaozhong@dync.cc>
2019-05-20 14:12:33 +0800

    update

--- app/src/main/java/org/ar/armeet/DeveloperInfo.java
+++ app/src/main/java/org/ar/armeet/DeveloperInfo.java
@@ -9,11 +9,11 @@ public class DeveloperInfo {
     /**
      * anyRTC 平台的appid
      */
-    public final static String APPID = "";
+    public final static String APPID = "anyrtcXJfqnOdWy8Di";
 
     /**
      * anyRTC 平台的apptoken
      */
-    public final static String APPTOKEN = "";
+    public final static String APPTOKEN = "007ac828e3ca883f6bab728018eec068";
 
 }
