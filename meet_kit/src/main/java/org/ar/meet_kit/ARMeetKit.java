319e7b06db13b462197f6a5bceaad61dd6675fe5
liuxiaozhong <liuxiaozhong@dync.cc>
2019-09-05 19:16:30 +0800

    更新

--- meet_kit/src/main/java/org/ar/meet_kit/ARMeetKit.java
+++ meet_kit/src/main/java/org/ar/meet_kit/ARMeetKit.java
@@ -931,7 +931,7 @@ public class ARMeetKit {
      * @param filePath 本地录像路径（音频文件格式为mp3, 视频文件格式为mp4）
      * @return 返回值：0：开始录制成功，-1：文件格式不匹配
      */
-    private int startRecorder(final boolean isNeedVideo, final boolean onlyLocalVideo, final String filePath) {
+    protected int startRecorder(final boolean isNeedVideo, final boolean onlyLocalVideo, final String filePath) {
         if(isNeedVideo) {
             //视频文件后缀名必须是mp4
             if(filePath.lastIndexOf("mp4") == -1) {
@@ -954,7 +954,34 @@ public class ARMeetKit {
         return LooperExecutor.exchange(result, 0);
     }
 
-
+    /**
+     * 开始录像
+     * @param isNeedVideo 是否需要录制视频（true：录制音视频， false：录制音频）
+     * @param filePath 本地录像路径（音频文件格式为mp3, 视频文件格式为mp4）
+     * @return 返回值：0：开始录制成功，-1：文件格式不匹配
+     */
+    public int startRecorder(final boolean isNeedVideo, final String filePath) {
+        if(isNeedVideo) {
+            //视频文件后缀名必须是mp4
+            if(filePath.lastIndexOf("mp4") == -1) {
+                return -1;
+            }
+        } else {
+            //音频文件后缀名必须是mp3
+            if(filePath.lastIndexOf("mp3") == -1) {
+                return -1;
+            }
+        }
+        final Exchanger<Integer> result = new Exchanger<Integer>();
+        mExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                int ret = nativeStartRecorder(isNeedVideo, true, filePath);
+                LooperExecutor.exchange(result, ret);
+            }
+        });
+        return LooperExecutor.exchange(result, 0);
+    }
 
     /**
      * 停止录像
